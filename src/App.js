9bfb5c58e4840e468b33d347ad9c4a161e8fc678
Tselmeg12 <dr.tse12@gmail.com>
2020-03-29 04:32:05 -0500

    dsfsdfdsf
    dsdfsdfdssdfsdfsdfsd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Facebook from './component/Facebook';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <p>Welcome to Poohh</p>
+       <p>Facebook auth example </p>
+       <Facebook></Facebook>
       </header>
     </div>
   );
