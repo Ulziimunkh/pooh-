aef3402e8c2e0fcbfc82cfce3c653ca82f5bedd1
Ulziimunkh <uujgii36@gmail.com>
2020-04-02 03:11:58 +0530

    scroll design zaslaa.

--- src/component/ChatView/styles.js
+++ src/component/ChatView/styles.js
@@ -1,13 +1,13 @@
 const styles = theme => ({
 
   content: {
-    height: 'calc(100vh - 100px)',
+    height: 'calc(100vh - 120px)',
     overflow: 'auto',
     padding: '25px',
     marginLeft: '300px',
     boxSizing: 'border-box',
     overflowY: 'scroll',
-    top: '50px',
+    top: '120px',
     width: 'calc(100% - 300px)',
     position: 'absolute'
   },
